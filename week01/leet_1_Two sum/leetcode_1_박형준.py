dc0717dd1afe23e556cbab1039077127c7b39cd5
phj9503 <phj9503@gmail.com>
2022-12-15 01:07:50 +0900

    hj_week01

--- week01/leet_1_Two sum/leetcode_1_박형준.py
+++ week01/leet_1_Two sum/leetcode_1_박형준.py
@@ -5,3 +5,28 @@ class Solution:
             for s_idx in range(f_idx + 1, nums_len):
                 if nums[f_idx] + nums[s_idx] == target:
                     return [f_idx, s_idx]
+
+# 시간 좀 더 줄인 것
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_len = len(nums)
+        for f_idx in range(nums_len):
+            s_num = target - nums[f_idx]
+            if s_num in nums[f_idx + 1:]:
+                return [f_idx, f_idx + 1 + nums[f_idx + 1:].index(s_num)]
+
+
+# solution 확인(시간복잡도 O(n))
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
