de087b3256975284a5463e9dc72c1a4a482bc6a2
phj9503 <phj9503@gmail.com>
2022-12-22 23:52:26 +0900

    hj_week02

--- week02/leet_234_Palindrome Linked List/leet_234_박형준.py
+++ week02/leet_234_Palindrome Linked List/leet_234_박형준.py
@@ -19,3 +19,35 @@ class Solution:
             if lst[:lst_l_2] == list(reversed(lst[lst_l_2 + 1:])):
                 return True
             return False
+
+# 더 간략한 풀이
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        q: Deque = collections.deque()
+        while head != None:
+            q.append(head.val)
+            head = head.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+        return True
+
+# 런너 풀이
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
